444407ca972a4dfbc49b99127353c275d3a9e281
Claire Fahy <zeebear21@gmail.com>
2019-04-17 12:12:06 -0300

    create new pics actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,4 +3,24 @@ class PicsController < ApplicationController
   def index
 
   end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic
+    else
+      render 'create'
+    end
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).require(:title, :description)
+  end
 end
