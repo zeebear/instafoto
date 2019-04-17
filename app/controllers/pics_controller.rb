78c570fd1aea7c858253cd75735f1a154bfc4f46
Claire Fahy <zeebear21@gmail.com>
2019-04-17 19:57:50 -0300

    tidying, bug zapping

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -45,6 +45,6 @@ class PicsController < ApplicationController
   end
 
   def pic_params
-    params.require(:pic).require(:title, :description)
+    params.require(:pic).permit(:title, :description, :photo)
   end
 end
